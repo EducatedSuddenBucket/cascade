d650b5e4512bcc8098b305f6fde1a7da9a71f613
EducatedSuddenBucket <educatedsuddenbucket@gmail.com>
2024-10-08 18:54:17 +0530

    Create roles.js

---
+++ commands/guild/roles.js
@@ -0,0 +1,32 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('roles')
+        .setDescription('Lists all roles in the server with their hex color code.'),
+
+    async execute(interaction) {
+        const { guild } = interaction;
+        const roles = guild.roles.cache.sort((a, b) => b.position - a.position);
+
+        const rolesEmbed = new EmbedBuilder()
+            .setColor(0x0099ff)
+            .setTitle(`Roles in ${guild.name}`)
+            .setThumbnail(guild.iconURL({ dynamic: true }));
+
+        let roleFields = [];
+        roles.forEach(role => {
+            roleFields.push({
+                name: role.name,
+                value: `ID: ${role.id} | Color: ${role.hexColor} | Members: ${role.members.size}`,
+                inline: false
+            });
+        });
+
+        rolesEmbed.addFields(roleFields);
+
+        await interaction.deferReply();
+        await interaction.editReply({ embeds: [rolesEmbed] });
+    },
+};
