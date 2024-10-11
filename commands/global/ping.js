6cf1f5d61e4e58ea044594092258e04ffc2c45f9
EducatedSuddenBucket <educatedsuddenbucket@gmail.com>
2024-10-11 13:09:01 +0530

    Update ping.js

--- commands/global/ping.js
+++ commands/global/ping.js
@@ -4,14 +4,13 @@ const { EmbedBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping')
-        .setDescription('Replies with the bot and API ping.'),
+        .setDescription('Replies with the bot and api Latency'),
     async execute(interaction) {
         const botPing = interaction.client.ws.ping; // Bot's WebSocket ping
         const apiPing = Date.now() - interaction.createdTimestamp; // API ping
 
         const embed = new EmbedBuilder()
-            .setColor('#0099ff') // Cool blue color
-            .setTitle('Ping Information')
+            .setColor('#0099ff')
             .addFields(
                 { name: 'Bot', value: `${botPing}ms`, inline: true },
                 { name: 'API', value: `${apiPing}ms`, inline: true }
